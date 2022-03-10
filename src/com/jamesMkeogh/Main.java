21f047eed0f0f9a4a4f03b912d553bc451bd373a
James Keogh <jkeogh@adga.ca>
2022-03-10 15:13:46 -0500

    improves output

--- src/com/jamesMkeogh/Main.java
+++ src/com/jamesMkeogh/Main.java
@@ -7,16 +7,21 @@ public class Main {
     public static void main(String[] args) {
         String[] pathnames;
         String fileToFind = "FK9DqsiXEAIRKXr.png";
-        File file = new File("C:\\Users\\james.keogh\\Desktop\\testImages");
+        String directory ="C:\\Users\\james.keogh\\Desktop\\testImages";
+        File file = new File(directory);
         pathnames = file.list();
 
         // Print the names of files and directories
+        findFiles(pathnames, fileToFind,directory);
+    }
+
+    private static void findFiles(String[] pathnames, String fileToFind,String directory) {
         if (pathnames != null) {
             for (String pathname : pathnames) {
                 System.out.println(pathname);
 
                 if (pathname.equals(fileToFind)) {
-                    System.out.println("found it " + pathname);
+                    System.out.println("Found it!!!!!  = " + pathname+" in "+directory);
                 }
             }
         }
